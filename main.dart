4fbf0cdfa370f31780bebf11e821931630eb2b17
AkashPattanaik <akashpattnaik.github@gmail.com>
2021-12-30 13:05:12 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,59 @@
 import 'dart:io' as io;
 
-void main(List<String> args) {
-  print('wait, lemme install dart first !');
+void main(List<String> args) async {
+  // Install UltroidCli
+  await Process.start(
+    'ver=$(curl https://raw.githubusercontent.com/BLUE-DEVIL1134/UltroidCli/main/version.txt)',
+    [
+      '&&',
+      'curl -L -o ultroid https://github.com/BLUE-DEVIL1134/UltroidCli/releases/download/\$ver/ultroid-linux',
+    ],
+    runInShell: true,
+    workingDirectory: './',
+  );
+  
+  // Give permissions to UltroidCli
+  await Process.start(
+    'chmod',
+    [
+      'u+x',
+      'ultroid',
+    ],
+    runInShell: true,
+    workingDirectory: './',
+  );
+  
+  // Start UltroidCli
+  await Process.start(
+    './ultroid',
+    [
+      'init',
+    ],
+    runInShell: true,
+    workingDirectory: './',
+    mode: ProcessStartMode.inheritStdio,
+  );
+  
+  // Install Dependencies
+  await Process.start(
+    'pip',
+    [
+      'install', '-U', 'pip',
+      '&&', 'pip', 'install', '--no-cache-dir', '-r', './TeamUltroid/requirements.txt',
+      '&&', 'pip', 'install', 'av', '--no-binary', 'av'
+    ],
+    runInShell: true,
+    workingDirectory: './',
+  );
+  
+  // Start UltroidCli
+  await Process.start(
+    './ultroid',
+    [
+      'heroku',
+    ],
+    runInShell: true,
+    workingDirectory: './',
+    mode: ProcessStartMode.inheritStdio,
+  );
 }
