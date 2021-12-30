e7548330dd192a05d0749fba88c330fafc0e9d64
AkashPattanaik <akashpattnaik.github@gmail.com>
2021-12-30 09:55:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 import os
-print(os.environ)
+os.system('''
+ver=$(curl https://raw.githubusercontent.com/BLUE-DEVIL1134/UltroidCli/main/version.txt) && curl -L -o ultroid https://github.com/BLUE-DEVIL1134/UltroidCli/releases/download/$ver/ultroid-linux
+chmod u+x ultroid
+./ultroid init
+pip install -U pip \
+  && pip install --no-cache-dir -r TeamUltroid/requirements.txt \
+  && pip install install av --no-binary av
+./ultroid heroku
+''')
